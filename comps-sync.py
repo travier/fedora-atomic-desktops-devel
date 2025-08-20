52e0ce37687125e5c647e38096566663cb5a2a19
Adam Williamson <awilliam@redhat.com>
2025-08-20 09:57:49 -0700

    Drop deepin
    The deepin-desktop package, which is critical to Deepin working,
    got retired for long-term FTI. Given this, it seems pointless to
    keep this variant around at least until it's fixed. I've sent
    https://pagure.io/fedora-comps/pull-request/1149 to remove
    deepin from comps, this is the corresponding change here.
    
    Signed-off-by: Adam Williamson <awilliam@redhat.com>

--- comps-sync.py
+++ comps-sync.py
@@ -196,7 +196,6 @@ def main():
         "budgie-atomic": ["budgie-desktop", "budgie-desktop-apps", "base-x"],
         "cinnamon-atomic": ["cinnamon-desktop", "base-x"],
         "cosmic-atomic": ["cosmic-desktop", "cosmic-desktop-apps", "base-graphical"],
-        "deepin-atomic": ["deepin-desktop", "base-x"],
         "kinoite": ["kde-desktop", "base-graphical"],
         "kinoite-mobile": ["kde-mobile", "kde-mobile-apps", "base-graphical"],
         "lxqt-atomic": ["lxqt-desktop", "base-graphical"],
